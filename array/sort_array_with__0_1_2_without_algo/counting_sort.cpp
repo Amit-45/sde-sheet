f79c4a94e9f91790e7eba38486e57a7264996e6f
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-05-01 10:16:30 +0530

    Update counting_sort.cpp

--- array/sort_array_with__0_1_2_without_algo/counting_sort.cpp
+++ array/sort_array_with__0_1_2_without_algo/counting_sort.cpp
@@ -1,4 +1,7 @@
-
+//This algorithm is used to sort an array of 0s 1s and 2s without the use of extra space or sorting algorithms
+//Not the most optimal solution
+//Other approaches-ARRAY SORTING or Dutch National flag algorithm
+//O(2n) time comlexity
 #include <bits/stdc++.h>
 using namespace std;
 int main()
@@ -13,30 +16,37 @@ int main()
     int c0 = 0;//count of 0
     int c1 = 0;//count of 1
     int c2 = 0;//count of 2
+
     for (i = 0; i < n; i++)
     {
         if (arr[i] == 0)
             c0++;
+
         if (arr[i] == 1)
             c1++;
+
         else
             c2++;
     }
+
+    int k=0;
     for (i = 0; i < c0; i++)
     {
-        arr[i] = 0;
+        arr[k++]=0;
     }
-    for (i = c0; i < c0 + c1; i++)
+    for (i = 0; i < c1; i++)
     {
-        arr[i] = 1;
+        arr[k++]=1;
     }
-    while(c2--)
+    for (i = 0; i < c2; i++)
     {
-        arr[i++]=2;
+        arr[k++]=2;
     }
+
     for (i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
+
     return 0;
 }
